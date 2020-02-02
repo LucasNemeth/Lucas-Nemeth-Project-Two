f0ea71981f53021d0f838aaf796d7696a88eeb31
LucasNemeth <l_orion_n@hotmail.com>
2020-02-02 16:24:58 -0500

    working on hamburger drop down menu

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+$(function () {
+    //----click on hamburger button to drop down the menu----//
+    $(".hamburger").click(function () {
+        // ---toggling class ----//
+        $(".top-nav").toggleClass('active');
+    })
+});
